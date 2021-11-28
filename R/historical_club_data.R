51da8df530bd2e917ee94d874444b13bf39e3d71
jvieroe <jvieroe@gmail.com>
2021-11-28 17:05:38 +0100

    hist_club_data

---
+++ R/historical_club_data.R
@@ -0,0 +1,53 @@
+#' name
+#'
+#' This function...
+#'
+#' @param x ..
+#' @return ...
+#' @author Jeppe Vierø
+#' @import RCurl
+#' @export
+
+historical_club_data <- function(leagues = NULL,
+                                 seasons = NULL) {
+
+  check_input(seasons = seasons,
+              leagues = leagues)
+
+  get_data <- base::expand.grid(leagues,
+                                seasons) %>%
+    tibble::tibble() %>%
+    dplyr::rename(league = Var1,
+                  season = Var2) %>%
+    dplyr::mutate(league_season = base::paste0(.data$league,
+                                               .data$season)) %>%
+    dplyr::mutate(url = base::paste0("https://www.football-data.co.uk/mmz4281/",
+                                     .data$season,
+                                     "/",
+                                     .data$league,
+                                     ".csv")) %>%
+    dplyr::mutate(url_exists = RCurl::url.exists(.data$url))
+
+  if(any(get_data$url_exists == FALSE)){
+    warning("Invalid league-season combinations provided, one or more URLs don't exist. Proceeding with valid combinations")
+  }
+
+  if(!any(get_data$url_exists == TRUE)){
+    stop("No valid league-season combinations provided, no URLs exist")
+  }
+
+  get_data <- get_data %>%
+    dplyr::filter(.data$url_exists == TRUE)
+
+  split_data <- base::split(get_data,
+                            f = get_data$league_season)
+
+  fd_data <- purrr::map(.x = split_data,
+                        .f = get_fun)
+
+  fd_data <- dplyr::bind_rows(fd_data) %>%
+    dplyr::filter(!is.na(HomeTeam) & !is.na(AwayTeam))
+
+  return(fd_data)
+
+}
---
+++ R/workhorse.R
@@ -0,0 +1,31 @@
+#' @noRd
+check_input <- function(seasons,
+                        leagues) {
+
+  if(is.null(seasons)){
+    stop("No seasons defined")
+  }
+
+  if(is.null(leagues)){
+    stop("No leagues defined")
+  }
+
+
+}
+
+#' @noRd
+check_url <- function(get_data) {
+
+  if(any(isFALSE(get_data$url))){
+    warning("Invalid league-season combinations provided, one or more URLs don't exist. Proceeding with filter")
+  }
+
+  if(!any(isTRUE(get_data$url))){
+    stop("No valid league-season combinations provided, no URLs exist")
+  }
+
+  # if(!RCurl::url.exists(url)){
+  #   stop("Invalid league(s)/season(s), URL doesn't exist")
+  # }
+
+}
