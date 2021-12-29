b1d59df55acbd3f862aa6bd6c106f7e29b037f90
jvieroe <jvieroe@gmail.com>
2021-12-29 23:53:18 +0100

    rename_vars

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,5 @@
 ## code to prepare `DATASET` dataset goes here
 
-#rename_vars <- readxl::read_xlsx("/Users/jeppeviero/github/rename_vars.xlsx")
-
-rename_vars <- c("HT" = "HomeTeam",
-                 "AT" = "AwayTeam")
+rename_vars <- readxl::read_xlsx("/Users/jeppeviero/github/rename_vars.xlsx")
 
 usethis::use_data(rename_vars, overwrite = TRUE)
