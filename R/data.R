5bf4d5118c495754ab3630277b099df68de3ca4b
jvieroe <jvieroe@gmail.com>
2021-12-30 02:14:43 +0100

    edits

--- R/data.R
+++ R/data.R
@@ -4,3 +4,15 @@
 #'
 #' @format A `tibble`
 "rename_vars"
+
+
+
+#' Variables to rename
+#'
+#' Variables to recode with old and new names
+#'
+#' @format A named `character vector`
+"lookup_vars"
+
+
+
